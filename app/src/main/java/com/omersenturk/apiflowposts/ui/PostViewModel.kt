fe4493d870856a66e9aee452190149d58ec4b488
Omer Senturk <omersenturk.dev@gmail.com>
2025-07-30 12:59:13 +0300

    feat(viewmodel): add PostViewModel for managing UI state

---
+++ app/src/main/java/com/omersenturk/apiflowposts/ui/PostViewModel.kt
@@ -0,0 +1,38 @@
+package com.omersenturk.apiflowposts.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.omersenturk.apiflowposts.data.PostRepository
+import com.omersenturk.apiflowposts.data.PostResponse
+import com.omersenturk.apiflowposts.data.PostUiModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PostViewModel @Inject constructor(
+    private val repository: PostRepository
+) : ViewModel() {
+
+    private val _posts = MutableStateFlow<Result<List<PostUiModel>>?>(null)
+    val posts: StateFlow<Result<List<PostUiModel>>?> = _posts
+
+    fun fetchPosts() {
+        viewModelScope.launch {
+            val result = repository.getPost()
+            _posts.value = result.map { posts -> mapToUiModel(posts) }
+        }
+    }
+
+    private fun mapToUiModel(posts: List<PostResponse>): List<PostUiModel> {
+        return posts.map { post ->
+            PostUiModel(
+                title = post.title,
+                body = post.body,
+                image = "https://picsum.photos/200/300?random=${post.id}"
+            )
+        }
+    }
+}
